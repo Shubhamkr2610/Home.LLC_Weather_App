17df0ebd778f4ac7ab204acce62f2210181f9331
shubhamoverdose <shubham@overdose.digital>
2024-02-18 02:47:13 +0530

    refactored code

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,63 +1,83 @@
 import React from "react";
+import { FiSunrise, FiSunset } from "react-icons/fi";
+import { LuWind } from "react-icons/lu";
+import {getFormattedDate} from '../scripts/utils'
+import {
+  LiaTemperatureHighSolid,
+  LiaTemperatureLowSolid,
+} from "react-icons/lia";
+import { WiHumidity } from "react-icons/wi";
 
-const WeatherCard = ({ temperature, windSpeed, sunCycle }) => {
-  const getFormattedDate = (timestamp) => {
-    const date = new Date(timestamp * 1000); // Multiply by 1000 to convert to milliseconds
+const WeatherCard = ({ temperature, windSpeed, sunCycle, weatherPng }) => {
 
-    let hours = date.getHours();
-    const minutes = date.getMinutes().toString().padStart(2, '0'); // Ensure two digits
-    const period = hours >= 12 ? 'PM' : 'AM';
-
-    // Convert hours from 24-hour format to 12-hour format
-    hours = (hours % 12) || 12;
-
-    return `${hours}:${minutes} ${period}`;
-  };
   return (
     <div className="w-full h-1/2 p-6 flex items-center justify-around gap-8 rounded-[30px] text-white bg-[#0f62fe]  shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
-      <div>
+      <div className="flex flex-col gap-6">
         <div>
           <p className="text-8xl font-bold">
             {Math.trunc(temperature?.temp)} &deg;C
           </p>
 
-          <p className="text-xl font-semibold">
+          <p className="text-xl font-semibold text-center">
             Feels like :{" "}
             <span className="text-4xl font-semibold">
               {Math.trunc(temperature?.feels_like)} &deg;C
             </span>{" "}
           </p>
         </div>
-        <div>
-          <div>
-            <p className="text-xl font-bold">Sunrise</p>
-            <p className="text-base font-semibold">
-              {getFormattedDate(sunCycle.sunrise)}
-            </p>
+        <div className="flex flex-col justify-center items-center gap-2">
+          <div className="flex items-center gap-2">
+            <FiSunrise size={"3em"} />
+            <div>
+              <p className="text-xl font-bold">Sunrise</p>
+              <p className="text-base font-semibold">
+                {getFormattedDate(sunCycle?.sunrise).time}
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="text-xl font-bold">Sunset</p>
-            <p className="text-base font-semibold"> {getFormattedDate(sunCycle.sunset)}</p>
+          <div className="flex items-center gap-2">
+            <FiSunset size={"3em"} />
+            <div>
+              <p className="text-xl font-bold">Sunset</p>
+              <p className="text-base font-semibold">
+                {" "}
+                {getFormattedDate(sunCycle?.sunset).time}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="text-4xl font-bold ">Sunny</div>
+      <div className="flex flex-col justify-center items-center gap-6">
+        <img
+          className="w-[120px] h-[120px] object-contain"
+          src={`http://openweathermap.org/img/w/${weatherPng[0].icon}.png`}
+        />
+        <p className="text-4xl font-semibold">{weatherPng[0].main}</p>
+      </div>
       <div className="h-full grid gap-6 grid-cols-2 items-center ">
-        <div className="text-center">
+        <div className="flex flex-col items-center">
+          <WiHumidity size={"3em"} />
           <p className="text-xl font-semibold">{temperature?.humidity}%</p>
           <p className="text-base font-medium">humidithy</p>
         </div>
-        <div className="text-center">
+        <div className="flex flex-col items-center">
+          <LuWind size={"3em"} />
           <p className="text-xl font-semibold">{windSpeed?.speed} km/h</p>
           <p className="text-base font-medium">windspeed</p>
         </div>
-        <div className="text-center">
-          <p className="text-xl font-semibold">{temperature?.pressure} hPa</p>
-          <p className="text-base font-medium">pressure</p>
+        <div className="flex flex-col items-center">
+          <LiaTemperatureHighSolid size={"3em"} />
+          <p className="text-xl font-semibold">
+            {Math.trunc(temperature?.temp_max)} &deg;C
+          </p>
+          <p className="text-base font-medium">Max temperature</p>
         </div>
-        <div className="text-center">
-          <p className="text-xl font-semibold">8</p>{" "}
-          <p className="text-base font-medium">uv</p>
+        <div className="flex flex-col items-center">
+          <LiaTemperatureLowSolid size={"3em"} />
+          <p className="text-xl font-semibold">
+            {Math.trunc(temperature?.temp_min)} &deg;C
+          </p>
+          <p className="text-base font-medium">Min temperature</p>
         </div>
       </div>
     </div>
