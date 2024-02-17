97950890d8db972aae520e5134d62c46a8fc1f65
shubhamoverdose <shubham@overdose.digital>
2024-02-18 02:46:57 +0530

    refactored code

--- src/App.js
+++ src/App.js
@@ -2,48 +2,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-import axios from "axios";
+
+import useWeatherHandler from "./scripts/useWeatherHandler";
 
 function App() {
   const [searchCity, setSearchCity] = useState("Patna");
   const [isLoading, setIsLoading] = useState(false);
-  const [wheatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
+  const getWeatherReport = useWeatherHandler()
 
-  const myDebounce = (fn, delay) => {
-    var timer;
-   if(timer) clearTimeout(timer);
-    timer = setTimeout(() => {
-      fn();
-    }, delay);
-
-    // return () => {
-    //   clearTimeout(timer);
-    // };
-  };
-  const getWeatherReport = async () => {
-    try {
-      setIsLoading(true);
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=0378eac035083a0436a721453604ca94&units=metric`
-      );
-      setWeatherData(response?.data);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  };
 
   useEffect(() => {
-    myDebounce(getWeatherReport, 3000);
+    getWeatherReport(searchCity,setWeatherData,setForecastData,setIsLoading)
   }, [searchCity]);
   return (
     <div className="bg-gradient-to-r from-white to-blue-200">
       <Header searchCity={searchCity} setSearchCity={setSearchCity} />
       {isLoading ? (
         <div>Loading...</div>
-      ) : !!wheatherData ? (
-        <HomePage wheatherData={wheatherData} />
+      ) : !!weatherData ? (
+        <HomePage weatherData={weatherData} forecastData={forecastData} />
       ) : (
         <h1>No city found. Try searching again!!</h1>
       )}
--- src/components/MainInfos.jsx
+++ src/components/MainInfos.jsx
@@ -1,61 +1,21 @@
 import React from "react";
-
+import { getFormattedDate } from "../scripts/utils";
 const MainInfos = ({ cityName, cityDateTime }) => {
-  const getFormattedDate = (timestamp) => {
-    const date = new Date(timestamp * 1000); // Multiply by 1000 to convert to milliseconds
-
-    let hours = date.getHours();
-    const minutes = date.getMinutes().toString().padStart(2, '0'); // Ensure two digits
-    const period = hours >= 12 ? 'PM' : 'AM';
-
-    // Convert hours from 24-hour format to 12-hour format
-    hours = (hours % 12) || 12;
-
-
-    // Get day, date, and month
-    const days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    const day = days[date.getDay()]; // Get the day of the week
-
-    const dateOfMonth = date.getDate().toString().padStart(2, "0"); // Ensure two digits
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const month = months[date.getMonth()]; // Get the month
-
-    return {
-      time: `${hours}:${minutes} ${period}`,
-      day: `${day}`,
-      date: `${dateOfMonth}`,
-      month: `${month}`,
-    };
-  };
   // text-[#c8d9f9]
   return (
     <div className="w-6/12 h-full p-6  flex flex-col items-center justify-center gap-8 rounded-[30px] text-white bg-[#0f62fe]  shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
       <h1 className="text-4xl font-bold">{cityName}</h1>
 
       <div className="flex flex-col items-center justify-center gap-3">
-        <h3 className="text-8xl font-bold"> {getFormattedDate(cityDateTime).time}</h3>
-        <h5 className="text-xl font-normal">{getFormattedDate(cityDateTime).day}, {getFormattedDate(cityDateTime).date}{getFormattedDate(cityDateTime).month}</h5>
+        <h3 className="text-8xl font-bold">
+          {" "}
+          {getFormattedDate(cityDateTime).time}
+        </h3>
+        <h5 className="text-xl font-normal">
+          {getFormattedDate(cityDateTime).day},{" "}
+          {getFormattedDate(cityDateTime).date}{" "}
+          {getFormattedDate(cityDateTime).month}
+        </h5>
       </div>
     </div>
   );
