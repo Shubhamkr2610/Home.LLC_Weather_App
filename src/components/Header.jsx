57ae9001a63f03c2e97c7eae673df1d515ec68d4
shubhamoverdose <shubham@overdose.digital>
2024-02-17 16:46:31 +0530

    implemented search fnctionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({searchCity, setSearchCity}) => {
   return (
-    <nav className="w-full h-[10vh] flex justify-center items-center">
-      <input className="w-2/5 h-10 px-6 rounded-3xl outline-none" placeholder="Search your preferred city"/>
+    <nav className="w-full h-[12vh] flex justify-center items-center ">
+      <input value={searchCity} onChange={(e)=>setSearchCity( e.target.value)} className="w-2/5 h-10 px-6 rounded-xl outline-none shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" placeholder="Search your preferred city" />
     </nav>
   );
 };
