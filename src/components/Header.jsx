7bba64924e21e42d0e142c9e0be349924a964a1f
shubhamoverdose <shubham@overdose.digital>
2024-02-18 02:45:58 +0530

    formatted code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Header = ({searchCity, setSearchCity}) => {
+const Header = ({ searchCity, setSearchCity }) => {
   return (
     <nav className="w-full h-[12vh] flex justify-center items-center ">
-      <input value={searchCity} onChange={(e)=>setSearchCity( e.target.value)} className="w-2/5 h-10 px-6 rounded-xl outline-none shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" placeholder="Search your preferred city" />
+      <input
+        value={searchCity}
+        onChange={(e) => setSearchCity(e.target.value)}
+        className="w-2/5 h-10 px-6 rounded-xl outline-none shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
+        placeholder="Search your preferred city"
+      />
     </nav>
   );
 };
