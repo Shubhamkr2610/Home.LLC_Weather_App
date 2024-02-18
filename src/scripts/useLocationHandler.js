fbb89fba3b447557151f7d0814d092b18e9ca8cf
shubhamoverdose <shubham@overdose.digital>
2024-02-18 23:41:54 +0530

    created useLocation hook

---
+++ src/scripts/useLocationHandler.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import {
+  GEO_CODING_API_KEY,
+  GEO_LOCATION_API_BASE_URL,
+  endpoints,
+} from "../constants";
+
+const useLocationHandler = () => {
+  const getCityByLatLong = async (latitude, longitude,setSearchCity,setIsLoading,setErrorMessage) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        `${GEO_LOCATION_API_BASE_URL}${endpoints.reverse}?lat=${latitude}&lon=${longitude}&appid=${GEO_CODING_API_KEY}`
+      );
+      if (response.status !== 200) {
+        return;
+      }
+      console.log(response);
+      setSearchCity(response?.data[0]?.name)
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error)
+
+      if(error?.response?.status == 401){
+        setErrorMessage(error.response.data.message);
+        setIsLoading(false);
+        return
+      }
+      setErrorMessage(error?.message);
+      setIsLoading(false);
+    }
+  };
+  return getCityByLatLong;
+};
+export default useLocationHandler;
