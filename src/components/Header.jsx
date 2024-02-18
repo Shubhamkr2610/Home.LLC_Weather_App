fec544330ec686de2a606ad0a764b24c25325b54
shubhamoverdose <shubham@overdose.digital>
2024-02-18 23:40:07 +0530

    refactored code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Header = ({ searchCity, setSearchCity }) => {
+const Header = ({ input, setInput }) => {
   return (
     <nav className="w-full h-[12vh] flex justify-center items-center ">
       <input
-        value={searchCity}
-        onChange={(e) => setSearchCity(e.target.value)}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         className="w-[90%] lg:w-2/5 h-10 px-6 rounded-xl outline-none shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
         placeholder="Search your preferred city"
       />
