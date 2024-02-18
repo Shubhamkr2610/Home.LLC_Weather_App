548f5ea8b48462573da4976c72e30fdec55d3089
shubhamoverdose <shubham@overdose.digital>
2024-02-18 13:13:41 +0530

    created and added loader component in home page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-
 import useWeatherHandler from "./scripts/useWeatherHandler";
+import Loader from "./components/Loader";
 
 function App() {
   const [searchCity, setSearchCity] = useState("Patna");
   const [isLoading, setIsLoading] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
-  const getWeatherReport = useWeatherHandler()
-
+  const getWeatherReport = useWeatherHandler();
 
   useEffect(() => {
-    getWeatherReport(searchCity,setWeatherData,setForecastData,setIsLoading)
+    getWeatherReport(searchCity, setWeatherData, setForecastData, setIsLoading);
   }, [searchCity]);
+
   return (
     <div className="bg-gradient-to-r from-white to-blue-200">
       <Header searchCity={searchCity} setSearchCity={setSearchCity} />
       {isLoading ? (
-        <div>Loading...</div>
+        <div className="h-[88vh] flex flex-col justify-center items-center bg-gradient-to-r from-white to-blue-200">
+          <Loader />
+        </div>
       ) : !!weatherData ? (
         <HomePage weatherData={weatherData} forecastData={forecastData} />
       ) : (
