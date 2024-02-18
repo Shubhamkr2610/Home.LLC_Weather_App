f5075d81ba6c20d0cc2df89c3b39a9be5013fa5b
shubhamoverdose <shubham@overdose.digital>
2024-02-18 14:15:09 +0530

    refactored css

--- src/components/FiveDaysForecast.jsx
+++ src/components/FiveDaysForecast.jsx
@@ -4,7 +4,6 @@ import { getFormattedDate } from "../scripts/utils";
 const FiveDaysForecast = ({ forecastData }) => {
   const uniqueForecastDays = new Set();
 
-  // Filter out unique forecast dates
   const uniqueForecastData = forecastData.list.filter((item) => {
     const forecastDate = new Date(item.dt_txt).getDate();
     if (!uniqueForecastDays.has(forecastDate)) {
@@ -15,21 +14,28 @@ const FiveDaysForecast = ({ forecastData }) => {
   });
 
   return (
-    <div className="w-full lg:w-6/12 h-full p-4 lg:p-6 flex flex-col items-center justify-start gap-2 rounded-[30px] text-white bg-[#0f62fe]  shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
-      <div className="text-3xl xl:text-4xl font-bold">{uniqueForecastData.slice(1).length} Days Forecast:</div>
-      {uniqueForecastData.slice(1).map((item, index) => {
-        return (
-          <div key={index} className="flex justify-center items-center gap-3">
-            <p className="text-lg xl:text-2xl font-bold">
-              {Math.trunc(item?.main?.temp)} &deg;C
-            </p>
-            <p className="text-base xl:text-xl font-bold">
-              {getFormattedDate(item.dt).day}, {getFormattedDate(item.dt).date}{" "}
-              {getFormattedDate(item.dt).month}
-            </p>
-          </div>
-        );
-      })}
+    <div className="w-full lg:w-6/12 h-full p-4 lg:p-6 flex flex-col items-center justify-start gap-2 rounded-[30px] text-white bg-[#0f62fe] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
+      <div className="text-3xl xl:text-4xl font-bold">
+        {uniqueForecastData?.slice(1)?.length} Days Forecast :
+      </div>
+
+      <div className="">
+        {uniqueForecastData?.slice(1)?.map((item, index) => {
+          return (
+            <div className="grid grid-cols-4 lg:grid-cols-2 justify-center items-center">
+            {/* flex justify-center items-center gap-3 */}
+              <p className=" text-lg xl:text-2xl font-bold text-center">
+                {Math.trunc(item?.main?.temp)} &deg;C
+              </p>
+              <p className="col-span-3 lg:col-span-1 text-base xl:text-xl font-bold text-center">
+                {getFormattedDate(item?.dt).day},{" "}
+                {getFormattedDate(item?.dt).date}{" "}
+                {getFormattedDate(item?.dt).month}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
