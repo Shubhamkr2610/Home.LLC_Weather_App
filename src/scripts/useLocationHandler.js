8ad197c7dbfff5646fdf2fae6a6dee960276dd94
shubhamoverdose <shubham@overdose.digital>
2024-02-19 15:45:19 +0530

    fix: added headers type

--- src/scripts/useLocationHandler.js
+++ src/scripts/useLocationHandler.js
@@ -6,23 +6,33 @@ import {
 } from "../constants";
 
 const useLocationHandler = () => {
-  const getCityByLatLong = async (latitude, longitude,setSearchCity,setIsLoading,setErrorMessage) => {
+  const getCityByLatLong = async (
+    latitude,
+    longitude,
+    setSearchCity,
+    setIsLoading,
+    setErrorMessage
+  ) => {
     try {
       setIsLoading(true);
       const response = await axios.get(
-        `${GEO_LOCATION_API_BASE_URL}${endpoints.reverse}?lat=${latitude}&lon=${longitude}&appid=${GEO_CODING_API_KEY}`
+        `${GEO_LOCATION_API_BASE_URL}${endpoints.reverse}?lat=${latitude}&lon=${longitude}&appid=${GEO_CODING_API_KEY}`,
+        {
+          headers: {
+            Accept: "application/json, text/plain, */*",
+          },
+        }
       );
       if (response.status !== 200) {
         return;
       }
-      setSearchCity(response?.data[0]?.name)
+      setSearchCity(response?.data[0]?.name);
       setIsLoading(false);
     } catch (error) {
-
-      if(error?.response?.status == 401){
+      if (error?.response?.status == 401) {
         setErrorMessage(error.response.data.message);
         setIsLoading(false);
-        return
+        return;
       }
       setErrorMessage(error?.message);
       setIsLoading(false);
