8aa5b83f8e4ec2b07ba3a8ec33a9326466de2888
shubhamoverdose <shubham@overdose.digital>
2024-02-19 16:08:12 +0530

    fix: base url

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const GEO_CODING_API_BASE_URL =
   "https://api.openweathermap.org/data/2.5";
 export const GEO_LOCATION_API_BASE_URL =
-  "http://api.openweathermap.org/geo/1.0";
+  "https://api.openweathermap.org/geo/1.0";
 export const GEO_CODING_API_KEY = "0378eac035083a0436a721453604ca94";
 
 export const endpoints = {
